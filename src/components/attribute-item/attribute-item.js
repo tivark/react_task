ad412f841ca73c9545dcca220d334faa1e076b6a
Vitali Krasutski <commmond@gmail.com>
2020-11-18 23:42:23 +0300

    Little refactoring

--- src/components/attribute-item/attribute-item.js
+++ src/components/attribute-item/attribute-item.js
@@ -1,17 +1,35 @@
-import React from "react";
-import {Draggable} from "react-beautiful-dnd";
+import React from 'react';
+import {Draggable} from 'react-beautiful-dnd';
 import {connect} from 'react-redux';
-import {attributeRelocate} from "../../actions";
+import {attributeRelocate} from '../../actions';
+import Paper from '@material-ui/core/Paper';
 
 import './attribute-item.css';
-import {IconButton} from "@material-ui/core";
+import {makeStyles} from '@material-ui/styles';
+import {commonStyles} from '../../styles/styles';
+import {IconButton} from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
 import DragIndicatorIcon from '@material-ui/icons/DragIndicator';
 
+const useStyles = makeStyles({
+  attributeItem: {
+    backgroundColor: commonStyles.backgroundColor,
+    marginBottom: '7px',
+    marginRight: '5px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    '&:hover .item-button': {
+      transition: 'opacity .2s',
+      opacity: '1'
+    }
+  }
+})
 
 const AttributeItem = (props) => {
   const {id, content, index, columnId} = props;
+  const classes = useStyles();
   const buttonIcon = columnId === 'available'
     ? <AddIcon/>
     : <DeleteIcon/>;
@@ -25,16 +43,19 @@ const AttributeItem = (props) => {
       {
         (provided) => {
           return (
-            <div className='attribute-item'
-                 {...provided.draggableProps}
-                 {...provided.dragHandleProps}
-                 ref={provided.innerRef}>
-              <DragIndicatorIcon />
+            <Paper className={classes.attributeItem}
+                   {...provided.draggableProps}
+                   {...provided.dragHandleProps}
+                   ref={provided.innerRef}>
+              <DragIndicatorIcon/>
               <div className='attribute-item__text'>{content}</div>
-              <IconButton onClick={onClickHandler} className='attribute-item__button'>
+              <IconButton
+                onClick={onClickHandler}
+                className='item-button'
+                disableFocusRipple>
                 {buttonIcon}
               </IconButton>
-            </div>
+            </Paper>
           )
         }
       }
