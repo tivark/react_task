428c8102cbd3482e651ff3eea9ea432ebf293b2c
Vitali Krasutski <commmond@gmail.com>
2020-11-23 11:50:23 +0300

    Removed extra files

--- src/components/beer-api-context/index.js
+++
@@ -1,6 +0,0 @@
-import { BeerDataProvider, BeerDataConsumer } from './beer-api-context';
-
-export {
-  BeerDataProvider,
-  BeerDataConsumer
-}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from "react-router-dom";
-import {Provider} from 'react-redux'
-
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux'
 import App from './components/app';
-import './index.css';
-
 import store from "./store";
-import {BeerDataProvider} from "./components/beer-api-context";
-import BeerService from "./services/beer-service";
-
-const beerService = new BeerService();
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BeerDataProvider value={beerService}>
-      <Router>
-        <App/>
-      </Router>
-    </BeerDataProvider>
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
